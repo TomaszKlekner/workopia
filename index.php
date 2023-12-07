9320d43f1c9e770d44ba288e5edee10438431720
Tomasz Klekner <tomasz.klekner@gmail.com>
2023-12-07 16:58:00 +0100

    Operators

--- index.php
+++ index.php
@@ -3,44 +3,25 @@ $title = 'Learn PHP From Scratch';
 $heading = 'Welcome To The Course';
 $body = 'In this course, you will learn the fundamentals of the PHP language';
 
-// Strings
+$output = null;
 
-$firstName = 'John';
-$lastName = 'Doe';
-$fullName = $firstName . ' ' . $lastName;
+$num1 = 20;
+$num2 = 10;
 
-// Implicit Type Conversion
-$number1 = 5;
-$number2 = 10;
-$number3 = '20';
-$fruit = 'apple';
-$bool1 = true;
-$bool2 = false;
-$null = null;
+// Basic Math Operators
+$output = '$num1 + $num2 = ' . $num1 + $num2;
+$output = '$num1 - $num2 = ' . $num1 - $num2;
+$output = '$num1 * $num2 = ' . $num1 * $num2;
+$output = '$num1 / $num2 = ' . $num1 / $num2;
+$output = '$num1 % $num2 = ' . $num1 % $num2;
 
-$result = $number1 + $number2;  // int
-$result = $number1 + $number3;  // int (string to int)
-$result = $number3 + $number3;  // int (string to int)
-$result = $number1 . $number2;  // string (int to string)
-// $result = $fruit + $number2; // Error
-$result = $number1 + $bool1;    // int (bool to int) true converted to 1
-$result = $number1 + $bool2;    // int (bool to int) false converted to 0
-$result = $number3 + $bool1;    // int (string to int) true converted to 1
-$result = $number3 + $bool2;    // int (string to int) false converted to 0
-$result = $number1 + $null;     // int (null to int) null converted to 0
-$result = $bool1 + $null;       // int (null to int) null converted to 0
-$result = $bool2 + $null;       // int (null to int) null converted to 0
+// Assignment Operators
+$num3 = 10;
+// $num3 = $num3 + 20;
+// $num3 += 20; // $num3 = $num3 + 10;
+$num3 -= 20; // $num3 = $num3 + 10;
 
-// Explicit Type Conversion
-$result = (string) $number1;    // int to string
-$result = (int) $number3;       // string to int
-$result = (bool) $number1;      // int to bool
-$result = (bool) $number2;      // int to bool
-$result = (bool) $number3;      // string to bool
-
-
-var_dump($result);
-
+$output = $num3;
 ?>
 
 <!DOCTYPE html>
@@ -62,9 +43,7 @@ var_dump($result);
   <div class="container mx-auto p-4 mt-4">
     <div class="bg-white rounded-lg shadow-md p-6">
       <h2 class="text-2xl font-semibold mb-4"><?= $heading ?></h2>
-      <p><?= $body ?></p'>
-      <p><?php echo "Hello, my name is $fullName"; ?></p>
-      <p><?= 'Hello, my name is ' . $fullName; ?></p>
+      <p><?= $output ?></p>
     </div>
   </div>
 </body>
