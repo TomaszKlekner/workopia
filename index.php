060538b2d1acd86773b4e7690989972145da29e0
Tomasz Klekner <tomasz.klekner@gmail.com>
2023-12-11 10:09:21 +0100

    Associative Arrays

--- index.php
+++ index.php
@@ -5,63 +5,22 @@ $body = 'In this course, you will learn the fundamentals of the PHP language';
 
 $output = null;
 
-$ids = [10, 25, 15, 45, 67];
-$users = ['user2', 'user1', 'user3'];
+$user = [
+  'name' => 'John',
+  'email' => 'john@mail.com',
+  'password' => '123456!',
+  'hobbies' => ['Tennis', 'Video Games'],
+];
 
-// Array methods
+$output = $user['name'];
+$output = $user['email'];
+// $output = $user['email2']; // Warning: Undefined array key "email2"
 
-// count()
-$output = count($ids);
+$output = $user['hobbies'][0];
 
-// sort()
-sort($ids);
-sort($users);
+$user['address'] = '123 Main Str';
+unset($user['address']);
 
-// rsort()
-rsort($ids);
-rsort($users);
-
-// array_push() - add to end
-array_push($ids, 100);
-array_push($users, 'user4');
-
-// array_pop() - remove last
-array_pop($ids);
-array_pop($users);
-
-// array_shift() - remove first
-array_shift($ids);
-array_shift($users);
-
-// array_unshift() - add as first
-array_unshift($ids, 333);
-array_unshift($users, 'user15');
-
-// array_slice() - returns a new array
-$ids2 = array_slice($ids, 2, 3);
-// var_dump($ids2);
-
-// array_splice() - replaces specified element, modifies the array directly
-array_splice($ids, 1, 1, 1000);
-array_splice($users, 1, 1, 'new user');
-
-// array_sum()
-$output = 'Sum of IDs: ' . array_sum($ids2);
-
-// array_search()
-$output = 'user1 is at index: ' . array_search('user1', $users);
-
-// in_array() - returns a bool value
-array_push($users, 'user3');
-$output = 'User 3 exists ' . in_array('user3', $users);
-
-// explode() - turn a string into an array
-$tags = 'tech, code, programming';
-$tagsArr = explode(',', $tags);
-//var_dump($tagsArr);
-
-// implode() - turn an array in a string
-$output = implode(', ', $users);
 
 ?>
 
@@ -85,15 +44,11 @@ $output = implode(', ', $users);
     <div class="bg-white rounded-lg shadow-md p-6">
       <h2 class="text-2xl font-semibold mb-4"><?= $heading ?></h2>
       <p><?php echo $output; ?></p>
-
-      <h2 class="text-xl font-semibold my-4">IDs Array:</h2>
+      <h2 class="text-xl font-semibold my-4">User Array:</h2>
       <p>
-      <pre><?php print_r($ids); ?></pre>
-      </p>
-
-      <h2 class="text-xl font-semibold my-4">Users Array:</h2>
-      <p>
-      <pre><?php print_r($users); ?></pre>
+      <pre>
+          <?php print_r($user); ?>
+        </pre>
       </p>
     </div>
   </div>
