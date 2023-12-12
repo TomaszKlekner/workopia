f867c65315c11543971c527a6a7c4ae987a4c0ce
Tomasz Klekner <tomasz.klekner@gmail.com>
2023-12-12 09:15:01 +0100

    Challenge 2

--- index.php
+++ index.php
@@ -11,9 +11,54 @@
 
 echo '<h3>Sum Of An Array</h3>';
 
-$numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+$numbers = [1, 2, 3, 4, 5];
 
 $sum = array_sum($numbers);
 $amount = count($numbers);
 
 echo "<p>The sum of the {$amount} numbers is: {$sum}</p>";
+
+/*
+  Challenge 2: Colors array
+
+  1. Reverse the '$colors' array
+  2. Add 'purple' and 'orange' to the end of the array
+  3. Replace the second color with 'pink'
+  4. Remove the last element of the array
+*/
+
+echo '<h3>Colors Array</h3>';
+
+$colors = ['red', 'blue', 'green', 'yellow'];
+
+// 1.
+$reversed_colors = array_reverse($colors);
+
+echo "<p>Reversed colors</p>";
+echo "<pre>";
+var_dump($reversed_colors);
+echo "</pre>";
+
+// 2.
+array_push($colors, 'purple', 'orange');
+
+echo "<p>More colors</p>";
+echo "<pre>";
+var_dump($colors);
+echo "</pre>";
+
+// 3.
+$colors[1] = "pink";
+
+echo "<p>Replace second color with 'pink'</p>";
+echo "<pre>";
+var_dump($colors);
+echo "</pre>";
+
+// 4.
+array_pop($colors);
+
+echo "<p>Remove last element</p>";
+echo "<pre>";
+var_dump($colors);
+echo "</pre>";
