73438d4568fb24ab5b264c0856f11a2ee66048bc
Tomasz Klekner <tomasz.klekner@gmail.com>
2023-12-27 14:39:16 +0100

    Challenge 2: Get The Sum Of An Array

--- index.php
+++ index.php
@@ -21,7 +21,9 @@ $second = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   </header>
 
   <div class="container mx-auto p-4 mt-4 col-auto">
-    <h2 class="text-2xl font-semibold mb-4">Challenge 1: Multiplication Table</h2>
+    <h2 class="text-2xl font-semibold mb-4">
+      Challenge 1: Multiplication Table
+    </h2>
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10">
       <?php foreach ($first as $f => $f_index) : ?>
         <div class="bg-white rounded-lg shadow-md p-6">
@@ -31,6 +33,28 @@ $second = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
           <?php endforeach; ?>
         </div>
       <?php endforeach; ?>
+    </div>
+  </div>
+
+  <div class="container mx-auto p-4 mt-4 col-auto">
+    <h2 class="text-2xl font-semibold mb-4">
+      Challenge 2: Get The Sum Of An Array
+    </h2>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10">
+      <div class="bg-white rounded-lg shadow-md p-6">
+        <?php
+        $my_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,];
+        $sum = 0;
+
+        foreach ($my_numbers as $number) {
+          $sum += $number;
+        }
+
+        echo "My numbers: " . implode(', ', $my_numbers) . "<br />";
+        echo "Sum of my numbers: " . $sum;
+
+        ?>
+      </div>
     </div>
   </div>
 </body>
