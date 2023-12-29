12ad72253085ebee7befe0802db89ad196fe2697
Tomasz Klekner <tomasz.klekner@gmail.com>
2023-12-29 13:20:11 +0100

    Functions with parameters and splat operator

--- index.php
+++ index.php
@@ -1,16 +1,33 @@
 <?php
 
-function sayHello()
+function add($a = 1, $b = 1)
 {
-  echo "Hello World";
+  return $a + $b;
 }
 
-function sayGoodbye()
+echo add(2, 2);
+echo '<br />';
+echo add();
+echo '<br />';
+
+function sayHello($name = "World")
 {
-  return "Goodbye!";
+  return "Hello " . $name;
 }
 
-// sayHello();
-$goodbye = sayGoodbye();
+echo sayHello("Tomek");
+echo '<br />';
+echo  sayHello();
 
-echo $goodbye;
+// Splat operator
+function addAll(...$numbers)
+{
+  $total = 0;
+  foreach ($numbers as $number) {
+    $total += $number;
+  }
+  return $total;
+}
+
+echo '<br />';
+echo addAll(1, 2, 3, 4, 5, 6, 7);
