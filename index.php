bd566acc4aa58d087635eb546b9e6844ec162190
Tomasz Klekner <tomasz.klekner@gmail.com>
2023-12-29 16:38:23 +0100

    Class example

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
 
 // Object Oriented Programming
+
+// Class example
+class User
+{
+  public $name;
+  public $email;
+
+  public function login()
+  {
+    echo "The user logged in";
+  }
+}
+
+// Instantiate a new User object
+$user_01 = new User();
+$user_01->name = "John Doe";
+$user_01->email = "test@gmail.com";
+
+var_dump($user_01);
+echo "<br />";
+
+$user_01->login();
