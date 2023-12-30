16557c4142171b388ad0d4da5691b7e36c197461
Tomasz Klekner <tomasz.klekner@gmail.com>
2023-12-30 17:22:55 +0100

    Class inheritance

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ class User
 {
   public $name;
   public $email;
-  private $status = 'active';
+  protected $status = 'active';
 
   public function __construct($name, $email)
   {
@@ -19,24 +19,29 @@ class User
   {
     echo $this->name . " logged in.<br />";
   }
+}
 
-  // Getter
+class Admin extends User
+{
+  public $level;
+
+  public function __construct($name, $email, $level)
+  {
+    $this->level = $level;
+    parent::__construct($name, $email);
+  }
+
+  public function login()
+  {
+    echo 'Admin ' . $this->name . " logged in.<br />";
+  }
+
   public function getStatus()
   {
     echo $this->status;
   }
-
-  // Setter
-  public function setStatus($status)
-  {
-    $this->status = $status;
-  }
 }
 
-// Instantiate a new User object
-$user_01 = new User("Tomasz", "tomasz@email.com");
+$admin_01 = new Admin("Tom Smith", "tom@email.com", 5);
 
-$user_01->login();
-echo "<br />";
-$user_01->setStatus("inactive");
-$user_01->getStatus();
+$admin_01->login();
