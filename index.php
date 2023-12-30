4bdb47e1f768f76c90636f1b09449662096fade4
Tomasz Klekner <tomasz.klekner@gmail.com>
2023-12-30 17:08:43 +0100

    Class example and object instance

--- index.php
+++ index.php
@@ -8,18 +8,21 @@ class User
   public $name;
   public $email;
 
+  public function __construct($name, $email)
+  {
+    $this->name = $name;
+    $this->email = $email;
+  }
+
   public function login()
   {
-    echo "The user logged in";
+    echo $this->name . " logged in.";
   }
 }
 
 // Instantiate a new User object
-$user_01 = new User();
-$user_01->name = "John Doe";
-$user_01->email = "test@gmail.com";
-
-var_dump($user_01);
-echo "<br />";
+$user_01 = new User("Tomasz", "tomasz@email.com");
 
 $user_01->login();
+echo "<br />.";
+var_dump($user_01);
