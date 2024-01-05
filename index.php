28213ea1010bced8ea5266db09fe7e7533cd3243
Tomasz Klekner <tomasz.klekner@gmail.com>
2024-01-05 14:34:45 +0100

    FILES super global - Upload files

--- index.php
+++ index.php
@@ -3,10 +3,36 @@ $title = '';
 $description = '';
 $submitted = false;
 
-// $_POST - data from Form
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
-  $title = htmlspecialchars($_POST['title']) ?? '';
-  $description = htmlspecialchars($_POST['description']) ?? '';
+  $title = htmlspecialchars($_POST['title'] ?? '');
+  $description = htmlspecialchars($_POST['description'] ?? '');
+  $file = $_FILES['logo'];
+
+  if ($file['error'] === UPLOAD_ERR_OK) {
+    // Specify where to upload
+    $uploadDir = 'uploads/';
+    // Check and create directory
+    if (!is_dir($uploadDir)) {
+      mkdir($uploadDir, 0755, true);
+    }
+    // Create file name
+    $filename = uniqid() . '-' . $file['name'];
+    // Check file type
+    $allowedExtensions = ['jpg', 'jpeg', 'png', 'svg'];
+    $fileExtension = strtolower(pathinfo($filename, PATHINFO_EXTENSION));
+    // Check if uploaded file has the allowed extensions
+    if (in_array($fileExtension, $allowedExtensions)) {
+      // Upload file
+      if (move_uploaded_file($file['tmp_name'], $uploadDir . $filename)) {
+        echo 'File Uploaded!';
+      } else {
+        echo 'File Upload Error: ' . $file['error'];
+      }
+    } else {
+      echo 'Invalid File Type';
+    }
+  }
+
   $submitted = true;
 }
 ?>
@@ -25,14 +51,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
   <div class="flex justify-center items-center h-screen">
     <div class="bg-white p-8 rounded shadow-md w-full max-w-md">
       <h1 class="text-2xl font-semibold mb-6">Create Job Listing</h1>
-      <form method="post">
+      <form method="post" enctype="multipart/form-data">
         <div class="mb-4">
           <label for="title" class="block text-gray-700 font-medium">Title</label>
-          <input type="text" id="title" name="title" placeholder="Enter job title" class="w-full px-4 py-2 border rounded focus:ring focus:ring-blue-300 focus:outline-none">
+          <input type="text" id="title" name="title" placeholder="Enter job title" class="w-full px-4 py-2 border rounded focus:ring focus:ring-blue-300 focus:outline-none" value="<?= $title ?>">
         </div>
         <div class="mb-6">
           <label for="description" class="block text-gray-700 font-medium">Description</label>
-          <textarea id="description" name="description" placeholder="Enter job description" class="w-full px-4 py-2 border rounded focus:ring focus:ring-blue-300 focus:outline-none"></textarea>
+          <textarea id="description" name="description" placeholder="Enter job description" class="w-full px-4 py-2 border rounded focus:ring focus:ring-blue-300 focus:outline-none"><?= $description ?></textarea>
+        </div>
+        <div class="mb-4">
+          <label for="resume" class="block text-gray-700 font-medium">Logo</label>
+          <input type="file" id="logo" name="logo" class="w-full px-4 py-2 border rounded focus:ring focus:ring-blue-300 focus:outline-none">
         </div>
         <div class="flex items-center justify-between">
           <button type="submit" name="submit" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none">
@@ -46,16 +76,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
       <?php if ($submitted) : ?>
         <div class="mt-6 p-4 border rounded bg-gray-200">
           <h2 class="text-lg font-semibold">Submitted Job Listing:</h2>
-          <p>
-            <strong>Title</strong>
-            <?= $title ?>
-          </p>
-          <p>
-            <strong>Description</strong>
-            <?= $description ?>
-          </p>
+          <p><strong>Title:</strong> <?= $title ?></p>
+          <p><strong>Description:</strong> <?= $description ?></p>
         </div>
-      <?php endif ?>
+      <?php endif; ?>
     </div>
   </div>
 </body>
