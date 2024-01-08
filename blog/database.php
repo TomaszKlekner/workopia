216656985f3dc0f6d2cdd81255924b66fa5a986a
Tomasz Klekner <tomasz.klekner@gmail.com>
2024-01-08 11:15:25 +0100

    Update db conn script

--- blog/database.php
+++ blog/database.php
@@ -15,12 +15,12 @@ $dsn = "mysql:host={$host};port={$port}:dbname={$dbName};charset=utf8";
 try {
   // Crate PDO instance
   $pdo  = new PDO($dsn, $username, $password);
-
   // Set POD to throw exceptions on error
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   // Fetch as Assoc array
   $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
   // If there is an error, catch it here
-  echo 'Connection Failde: ' . $e->getMessage();
+  echo 'Connection Failed: ' . $e->getMessage();
+  exit;
 }
