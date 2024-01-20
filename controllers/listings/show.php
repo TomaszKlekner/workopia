f821c5156c44fc1ca43b4021e7467fb2c8caa3ad
Tomasz Klekner <tomasz.klekner@gmail.com>
2024-01-20 21:24:51 +0100

    Single listing view

---
+++ controllers/listings/show.php
@@ -0,0 +1,16 @@
+<?php
+$config = require basePath('config/db.php');
+$db = new Database($config);
+
+// Get the listing id passed as a query parameter
+$id = $_GET['id'] ?? "";
+
+$params = [
+  'id' => $id
+];
+
+$listing = $db->query('SELECT * FROM workopia.listings where id = :id', $params)->fetch();
+
+loadView('listings/show', [
+  'listing' => $listing
+]);
