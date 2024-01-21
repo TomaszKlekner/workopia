c8e4a1c824a970b12623e25198a059a1b4e808c5
Tomasz Klekner <tomasz.klekner@gmail.com>
2024-01-21 21:30:56 +0100

    Update folder structure

---
+++ App/views/error/403.view.php
@@ -0,0 +1,17 @@
+<?php
+loadPartial("head");
+loadPartial("navbar");
+?>
+
+<section class="flex-auto flex justify-center align-center">
+  <div class="container grow-0 mx-auto p-4 my-4">
+    <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">403 Error</div>
+    <p class="text-center text-2xl mb-4">
+      You are not authorized to view this page!
+    </p>
+  </div>
+</section>
+
+<?php
+loadPartial("footer");
+?>
