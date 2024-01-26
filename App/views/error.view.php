2f43aa9daf376d76cc3c233e19d5eeebb12bfc7d
Tomasz Klekner <tomasz.klekner@gmail.com>
2024-01-26 23:48:30 +0100

    Create and use an error controller

--- App/views/error.view.php
+++ App/views/error.view.php
@@ -5,10 +5,8 @@ loadPartial("navbar");
 
 <section class="flex-auto flex justify-center align-center">
   <div class="container grow-0 mx-auto p-4 my-4">
-    <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">404 Error</div>
-    <p class="text-center text-2xl mb-4">
-      This page does not exist
-    </p>
+    <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3"><?= $status ?></div>
+    <p class="text-center text-2xl mb-4"><?= $message ?></p>
   </div>
 </section>
 
--- Framework/Router.php
+++ Framework/Router.php
@@ -2,6 +2,8 @@
 
 namespace Framework;
 
+use App\Controllers\ErrorController;
+
 class Router
 {
   protected $routes = [];
@@ -24,19 +26,6 @@ class Router
       'controller' => $controller,
       'controllerMethod' => $controllerMethod
     ];
-  }
-
-  /**
-   * Load error page
-   * 
-   * @param int $httpCode
-   * @return void
-   */
-  public function error($httpCode = 404)
-  {
-    http_response_code($httpCode);
-    loadView("error/{$httpCode}");
-    exit;
   }
 
   /**
@@ -109,6 +98,6 @@ class Router
       }
     }
 
-    $this->error();
+    ErrorController::notFound();
   }
 }
