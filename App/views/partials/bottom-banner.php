1ba68639124ccafaf853b20813f3cdbf946aa608
Tomasz Klekner <tomasz.klekner@gmail.com>
2024-02-11 13:38:45 +0100

    Update bottom banner

--- App/views/partials/bottom-banner.php
+++ App/views/partials/bottom-banner.php
@@ -1,14 +1,14 @@
 <!-- Bottom Banner -->
 <section class="container mx-auto my-6">
-  <div class="bg-blue-800 text-white rounded p-4 flex items-center justify-between">
-    <div>
+  <div class="bg-blue-800 text-white md:rounded p-4 flex items-center justify-between">
+    <div class="me-1 sm:me-0">
       <h2 class="text-xl font-semibold">Looking to hire?</h2>
       <p class="text-gray-200 text-lg mt-2">
         Post your job listing now and find the perfect candidate.
       </p>
     </div>
-    <a href="/listings/create" class="bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded hover:shadow-md transition duration-300">
-      <i class="fa fa-edit"></i> Post a Job
+    <a href="/listings/create" class="flex items-center justify-between bg-yellow-500  hover:bg-yellow-600 text-black px-4 py-2 rounded hover:shadow-md transition duration-300">
+      <i class="fa fa-edit"></i> <span class="block ms-2 me-4 shrink-0">Post a Job</span>
     </a>
   </div>
 </section>
