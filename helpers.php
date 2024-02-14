da30042b79f660a8f84317b1fa100ed92fec8324
Tomasz Klekner <tomasz.klekner@gmail.com>
2024-02-14 12:50:07 +0100

    Refactor the loadPartial method

--- helpers.php
+++ helpers.php
@@ -32,12 +32,14 @@ function loadView($name, $data = [])
  * Load a partial
  * 
  * @param string $name
+ * @param string $data
  * @return void
  */
-function loadPartial($name)
+function loadPartial($name, $data = [])
 {
   $partialPath = basePath("App/views/partials/{$name}.php");
   if (file_exists($partialPath)) {
+    extract($data);
     require $partialPath;
   } else {
     echo "Partial '{$name}' not found!";
